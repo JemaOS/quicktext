d81a9a8f0b47212c4726bff1e74c24befe975250
johnkryptochain <jonathan@jematechnology.fr>
2026-02-23 16:17:24 +0100

    Fix SonarQube: add comments to externs functions

--- js/background_externs.js
+++ js/background_externs.js
@@ -5,20 +5,27 @@
  * These are type declarations, not implementations.
  * @constructor
  */
-function TextApp() {}
+function TextApp() {
+  // Externs constructor - intentionally empty for Closure Compiler type declarations
+}
 /**
  * @param {boolean} v
  */
-TextApp.prototype.setHasChromeFrame = function(v) {};
+TextApp.prototype.setHasChromeFrame = function(v) {
+  // Externs method - intentionally empty for Closure Compiler type declarations
+};
 /**
  * @param {Array.<FileEntry>} entries
  */
-TextApp.prototype.openTabs = function(entries) {};
+TextApp.prototype.openTabs = function(entries) {
+  // Externs method - intentionally empty for Closure Compiler type declarations
+};
 /**
  * @return {Array.<FileEntry>}
  */
-TextApp.prototype.getFilesToRetain = function() {};
-
+TextApp.prototype.getFilesToRetain = function() {
+  // Externs method - intentionally empty for Closure Compiler type declarations
+};
 /**
  * @type {TextApp}
  */
